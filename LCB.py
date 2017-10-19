39b0719d4f25c9edc011240354ddc81f087104b1
Michael Hayes <Michael.hayes.2013@live.rhul.ac.uk>
2017-10-19 08:27:15 +0100

    Add files via upload

--- LCB.py
+++ LCB.py
@@ -10,46 +10,48 @@ _FPACol_ = 4
 
 _MINSIGNALERR_ = 5
 _MINFPAERR_ = 0.005
+_removeHeatup = False
+
 
 def run(filepath):
-    outname = filepath.split("\\")[-1].replace("RAW.txt","PROCESSED.txt")
-    outputpath = os.path.dirname(os.path.realpath(filepath)) + "\\" + outname
-
     #get binned data
-    binnedData = getBinnedData(filepath)
+    data = getData(filepath)
+    if _removeHeatup:
+        outname = filepath.split("\\")[-1].replace("RAW","RAW_NO_HEATING")
+        outputpath = os.path.dirname(os.path.realpath(filepath)) + "\\" + outname
+        writeToFile2d(outputpath,data)
+        print("File written to " + outputpath)
+
     processedData = []
     processedData.append(['#ITS90 Temp\tITS90 Stdev\tSignal\tSignal Stdev\tFPA Temp\tFPA Stdev\tWeight\tn'])
-    for bin in binnedData:
-        datapoint = produceDatapoint(bin)
+    for setpoint in data:
+        datapoint = produceDatapoint(setpoint)
         if datapoint == None:
             pass
         else:
             processedData.append(datapoint)
 
-    sortAscendingData(processedData)
-    applyWeights(processedData)
+    outname = filepath.split("\\")[-1].replace("RAW","PROCESSED")
+    outputpath = os.path.dirname(os.path.realpath(filepath)) + "\\" + outname
+    writeToFile(outputpath,processedData)
+    print("File written to " + outputpath)
 
+def writeToFile(outputpath, data):
     f = file(outputpath,'w')
-    for point in processedData:
+    for point in data:
         for col in point:
             f.write(str (col) + "\t")
         f.write("\n")
     f.close()
 
-    print("File written to " + outputpath)
-
-def sortAscendingData(processedData):
-    #sort in terms of increasing FPA Temperature for each F250 setpoint
-    nChanges = 1
-    while nChanges>0:
-        nChanges = 0
-        for i in range(1,len(processedData)-1):
-            if abs(processedData[i][0]-processedData[i+1][0])<1:
-                if processedData[i][4]>processedData[i+1][4]:
-                    temp1 = processedData[i]
-                    processedData[i] = processedData[i+1]
-                    processedData[i+1] = temp1
-                    nChanges+=1
+def writeToFile2d(outputpath, data):
+    f = file(outputpath, 'w')
+    for setpoint in data:
+        for point in setpoint:
+            for col in point:
+                f.write(str(col) + "\t")
+            f.write("\n")
+    f.close()
 
 def applyWeights(processedData):
     tempIndex = 1
@@ -67,8 +69,6 @@ def applyWeights(processedData):
 
         for i in range(startVal,endVal):
             processedData[i][6] = 1.0/(endVal-startVal)
-
-
 
 def produceDatapoint(FPABin):
     if (len(FPABin))==0:
@@ -91,13 +91,13 @@ def getAverageAndStdev(array,col,minErr = 0):
     if len(array)>1:
         for point in array:
             sigma+=(point[col]-mean)**2
-        sigma = sigma**0.5
         sigma/=(len(array)-1)
+        sigma = sigma**0.5
     else:
         sigma = minErr
     return mean,sigma
 
-def getBinnedData(filepath):
+def getData(filepath):
     #binned[i] = arrays of data grouped to same F250 temperature
     #binned[i][j] = array binned to same F250 temp and similar FPA temperature
     #binned[i][j][k] = array corresponding to one measurement
@@ -106,41 +106,41 @@ def getBinnedData(filepath):
     for row in raw:
         data.append(np.ndarray.tolist(row))
     sortedData = sortData(data)
-    binnedData = []
-    for elem in sortedData:
-        #==================================
-        #fudgery located here- possibly fixed?
-        binned = binData(elem)
-        for arr in binned:
-            binnedData.append(arr)
-        #==================================
-    return binnedData
+    if _removeHeatup:
+        for i in range(len(sortedData)):
+            sortedData[i] = removeHeatupPeriod(sortedData[i])
+    return sortedData
+
+def removeHeatupPeriod(measurementSet):
+    if len(measurementSet)<2:
+        return measurementSet
+    prevFPA = 0
+    startVal = 0
+    medianValue = measurementSet[len(measurementSet)/2][_FPACol_]
+    while abs(measurementSet[startVal][_FPACol_]-medianValue)>0.05 and startVal<len(measurementSet)-1:
+        startVal+=1
+    measurementSet = measurementSet[startVal:-1]
+    return measurementSet
 
 def sortData(data):
+    #sortAscendingData(data)
     sortedData = []
     index = 0
     while index<len(data)-1:
         bin = []
         prevF250Temp = data[index][_ITS90Col_]
         prevFPATemp = data[index][_FPACol_]
+        #TODO: change this here
         while data[index][_ITS90Col_]==prevF250Temp and index<len(data)-1 and abs(data[index][_FPACol_]-prevFPATemp)<1.5:
             prevF250Temp = data[index][_ITS90Col_]
             prevFPATemp = data[index][_FPACol_]
             bin.append(data[index])
             index+=1
+        if len(bin)==1:
+            print "uhoh"
+            print(data[index],data[index-1],index)
         sortedData.append(bin)
     return sortedData
-
-def binData(sortedDataElem,nBins = 10):
-    #return a list of bins containing points with similar FPA temperature
-    min,max = getMinMaxFPATemp(sortedDataElem)
-    binnedData = []
-    for i in range(nBins):
-        binnedData.append([])
-    for row in sortedDataElem:
-        binIndex = int ((row[_FPACol_]-min)/nBins)
-        binnedData[binIndex].append(row)
-    return binnedData
 
 def getMinMaxFPATemp(sortedDataElem):
     min = 100
@@ -153,7 +153,7 @@ def getMinMaxFPATemp(sortedDataElem):
             max = FPATemp
     return min,max
 
-run('D:\\Lepton\\Data\\DEC_16_STABILITY_RAW.txt')
-run('D:\\Lepton\\Data\\DEC_16_CALIB_RAW.txt')
-run('D:\\Lepton\\Data\\APR_17_STABILITY_RAW.txt')
-run('D:\\Lepton\\Data\\APR_17_CALIB_RAW.txt')
+run('D:\\Lepton\\Data\\RAW_NO_HEATING_DEC_16_STABILITY.txt')
+run('D:\\Lepton\\Data\\RAW_NO_HEATING_DEC_16_CALIB.txt')
+run('D:\\Lepton\\Data\\RAW_NO_HEATING_APR_17_STABILITY.txt')
+run('D:\\Lepton\\Data\\RAW_NO_HEATING_APR_17_CALIB.txt')
